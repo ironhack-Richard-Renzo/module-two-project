2961a57d0586f5dde67d479cc9474f44ed46349b
Liu Richard <rliu5694@gmail.com>
2021-01-30 12:02:23 +0100

    fix:login

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/christmas-project';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/module-two-project';
 
 mongoose
     .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
