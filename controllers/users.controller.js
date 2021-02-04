5e3e5f0b0b9931cf83d5b04c5ec5aa6ee18f690a
Renzo <renzo.roca.m@gmail.com>
2021-02-04 20:22:27 +0100

    add to whislist feat done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,81 +5,77 @@ const mailer = require('../config/mailer.config');
 const passport = require('passport');
 
 module.exports.register = (req, res, next) => {
-  res.render('users/register', { layout:'loginLayout.hbs' });
+    res.render('users/register', { layout: 'loginLayout.hbs' });
 };
 
 module.exports.doRegister = (req, res, next) => {
-  function renderWithErrors(errors) {
-    res.status(400).render('users/register', {
-      layout:'loginLayout.hbs',
-      user: req.body,
-      errors: errors,
-    });
-  }
-
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (user) {
-        renderWithErrors({ email: 'Email is already registered ' });
-      } else {
-        return User.create(req.body).then((user) => {
-          mailer.sendValidationEmail(user.email, user.verified.token, user.name);
-
-          res.render('users/login', { layout:'loginLayout.hbs', verification: true });
+    function renderWithErrors(errors) {
+        res.status(400).render('users/register', {
+            layout: 'loginLayout.hbs',
+            user: req.body,
+            errors: errors,
         });
-      }
-    })
-    .catch((error) => {
-      if (error instanceof mongoose.Error.ValidationError) {
-        renderWithErrors(error.errors);
-      } else {
-        next(error);
-      }
-    });
+    }
+
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (user) {
+                renderWithErrors({ email: 'Email is already registered ' });
+            } else {
+                return User.create(req.body).then((user) => {
+                    mailer.sendValidationEmail(user.email, user.verified.token, user.name);
+
+                    res.render('users/login', { layout: 'loginLayout.hbs', verification: true });
+                });
+            }
+        })
+        .catch((error) => {
+            if (error instanceof mongoose.Error.ValidationError) {
+                renderWithErrors(error.errors);
+            } else {
+                next(error);
+            }
+        });
 };
 
 module.exports.login = (req, res, next) => {
-  res.render('users/login', {layout: 'loginLayout.hbs'});
+    res.render('users/login', { layout: 'loginLayout.hbs' });
 };
 
 module.exports.doLogin = (req, res, next) => {
-  passport.authenticate('local-auth', (error, user, validations) => {
-    if (error) {
-      next(error);
-    } else if (!user) {
-      res.status(400).render('users/login', { user: req.body, errors: validations });
-    } else {
-      req.login(user, error => {
-        if (error) next(error)
-        else res.redirect('/')
-      })
-    }
-  })(req, res, next);
+    passport.authenticate('local-auth', (error, user, validations) => {
+        if (error) {
+            next(error);
+        } else if (!user) {
+            res.status(400).render('users/login', { user: req.body, errors: validations });
+        } else {
+            req.login(user, error => {
+                if (error) next(error)
+                else res.redirect('/')
+            })
+        }
+    })(req, res, next);
 };
 
 module.exports.logout = (req, res, next) => {
-  req.logout();
-  res.redirect('/login');
+    req.logout();
+    res.redirect('/login');
 };
 
 module.exports.activate = (req, res, next) => {
-  User.findOneAndUpdate(
-    { 'verified.token': req.query.token },
-    { $set: { verified: { date: new Date(), token: null } } },
-    { runValidators: true }
-  ).then(user => {
-    if (!user) {
-      next(httpError(404, 'Invalid activation token'))
-    } else {
-      res.redirect('/login');
-    }
-  }).catch(next);
+    User.findOneAndUpdate({ 'verified.token': req.query.token }, { $set: { verified: { date: new Date(), token: null } } }, { runValidators: true }).then(user => {
+        if (!user) {
+            next(httpError(404, 'Invalid activation token'))
+        } else {
+            res.redirect('/login');
+        }
+    }).catch(next);
 };
 
 module.exports.profile = (req, res, next) => {
-  User.findById(req.user.id)
-  .then(user => res.render('users/profile', {user}))
-  .catch(next)
+    User.findById(req.user.id)
+        .then(user => res.render('users/profile', { user }))
+        .catch(next)
 }
 
 // module.exports.doProfile = (req, res, next) => {
@@ -91,3 +87,32 @@ module.exports.profile = (req, res, next) => {
 //     });
 //   }
 // }
+
+
+
+
+
+module.exports.addToWhishList = (req, res, next) => {
+    User.findOneAndUpdate({ _id: req.user.id }, { $push: { wishlist: req.params.id } }, { new: true }).then(user => {
+        if (!user) {
+            next(httpError(404, 'Invalid user'))
+        } else {
+            console.log('user after add pop => ', user);
+            res.redirect('/products');
+        }
+    }).catch(next);
+}
+
+module.exports.populateWishList = (req, res, next) => {
+
+    User.findById('6019a0b2030c101a74ad7634').populate('wishlist')
+        .then((user) => {
+            if (user) {
+                console.log('el user => ', user);
+                res.render('users/wishlist', { user });
+            } else {
+                res.redirect('/products');
+            }
+        }).catch(next);
+
+}
--- models/user.model.js
+++ models/user.model.js
@@ -4,68 +4,80 @@ const bcrypt = require('bcrypt');
 const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_PATTERN = /^.{8,}$/;
 
-const userSchema = new Schema(
-  {
+const userSchema = new Schema({
     name: {
-      type: String,
-      required: 'User name is mandatory',
-      trim: true,
+        type: String,
+        required: 'User name is mandatory',
+        trim: true,
     },
     description: {
-      type: String,
+        type: String,
     },
     email: {
-      type: String,
-      required: 'Email is required',
-      match: [EMAIL_PATTERN, 'Invalid email'],
-      unique: true,
-      lowercase: true,
-      trim: true,
+        type: String,
+        required: 'Email is required',
+        match: [EMAIL_PATTERN, 'Invalid email'],
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
     password: {
-      type: String,
-      required: 'Password is required',
-      match: [PASSWORD_PATTERN, 'Password needs at least 8 chars'],
+        type: String,
+        required: 'Password is required',
+        match: [PASSWORD_PATTERN, 'Password needs at least 8 chars'],
     },
     verified: {
-      date: Date,
-      token: {
-        type: String,
-        default: () =>
-          Math.random().toString(36).substr(2) +
-          Math.random().toString(36).substr(2) +
-          Math.random().toString(36).substr(2) +
-          Math.random().toString(36).substr(2) +
-          Math.random().toString(36).substr(2),
-      },
+        date: Date,
+        token: {
+            type: String,
+            default: () =>
+                Math.random().toString(36).substr(2) +
+                Math.random().toString(36).substr(2) +
+                Math.random().toString(36).substr(2) +
+                Math.random().toString(36).substr(2) +
+                Math.random().toString(36).substr(2),
+        },
     },
     avatar: {
-      type: String,
-      default: function() {
-        return `https://i.pravatar.cc/150?u=${this.id}`
-      }
+        type: String,
+        default: function() {
+            return `https://i.pravatar.cc/150?u=${this.id}`
+        }
     },
-    wishlist: [{
-      type: Schema.Types.ObjectId,
-      rel: 'Product'
-    }]
-  },
-  { timestamps: true },
-);
-
-userSchema.pre('save', function (next) {
-  if (this.isModified('password')) {
-    bcrypt.hash(this.password, 10).then((hash) => {
-      this.password = hash;
-      next();
-    });
-  } else {
-    next();
-  }
+    wishlist: {
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }]
+    }
+}, {
+    timestamps: true,
+    toObject: {
+        virtuals: true,
+        transform: (doc, ret) => {
+            ret.id = doc._id;
+            delete ret._id;
+            delete ret.__v;
+            delete ret.createdAt;
+            delete ret.updatedAt;
+            return ret;
+        }
+    }
 });
 
-userSchema.methods.checkPassword = function (passwordToCheck) {
-  return bcrypt.compare(passwordToCheck, this.password);
+userSchema.pre('save', function(next) {
+    if (this.isModified('password')) {
+        bcrypt.hash(this.password, 10).then((hash) => {
+            this.password = hash;
+            next();
+        });
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.checkPassword = function(passwordToCheck) {
+    return bcrypt.compare(passwordToCheck, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
