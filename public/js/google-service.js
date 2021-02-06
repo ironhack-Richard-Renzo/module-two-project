3f32bbbc402e195040ab29ddbd93f56ce8261cc0
Renzo <renzo.roca.m@gmail.com>
2021-02-06 13:24:35 +0100

    refactorize google service

--- public/js/google-service.js
+++ public/js/google-service.js
@@ -1,19 +1,34 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+    const madrid = { lat: 40.416775, lng: -3.703790 };
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
-        center: uluru,
+        center: madrid,
+        zoom: 18,
+        zoomControl: true,
+        mapTypeId: 'satellite',
+        heading: 90,
+        tilt: 0,
+        disableDefaultUI: true
     });
-    // The marker, positioned at Uluru
+
+    input = document.getElementById('places-input');
+    autocomplete = new google.maps.places.Autocomplete(this.input);
+    autocomplete.addListener('place_changed', () => {
+        const place = this.autocomplete.getPlace();
+        onPlaceChanged(place);
+    });
+
     const marker = new google.maps.Marker({
-        position: uluru,
+        position: madrid,
         map: map,
     });
 }
 
-
-
-//module.exports = initMap();
+function onPlaceChanged(place) {
+    currentUser.latitude = place.geometry.location.lat();
+    currentUser.longitude = place.geometry.location.lng();
+    this.map.panTo(place.geometry.location);
+    this.map.setZoom(18);
+    this.marker.setPosition(place.geometry.location);
+}
